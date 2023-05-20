7466705f5b5ae21e8a260f293ac89647117e0f97
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-05-20 13:53:27 -0400

    Fix bug img not showing

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,7 +1,9 @@
 import Ingredient from "./Ingredient";
+import milkImg from "../assets/milk.png";
+import spiceImg from "../assets/spice.png";
 
-export const spice = new Ingredient("i1", 52, 55, 50, 50, "src/assets/spice.png", 50);
-export const milk = new Ingredient("i2", 150, 55, 50, 50, "src/assets/milk.png", -10);
+export const spice = new Ingredient("i1", 52, 55, 50, 50, spiceImg, 50);
+export const milk = new Ingredient("i2", 150, 55, 50, 50, milkImg, -10);
 
 export const ingredients = [spice, milk];
 
